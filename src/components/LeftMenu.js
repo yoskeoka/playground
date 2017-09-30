563d7587c0dacae14a1de3f134639e98bf0bc62a
yoskeoka <yoske95@gmail.com>
2017-10-01 01:38:26 +0900

    add drawer menu sample

---
+++ src/components/LeftMenu.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+
+const styles = {
+  list: {
+    width: 250,
+    flex: 'initial',
+  }
+};
+
+class LeftMenu extends React.Component {
+  render(){
+      return (
+        <Drawer open={this.props.open} onRequestClose={this.props.handleClose}>
+          <List className={this.props.classes.list}>
+            <ListItem button onClick={()=>{this.props.handleSelectMenu('page1')}}>
+              <ListItemText primary="Page1" />
+            </ListItem>
+            <ListItem button onClick={()=>{this.props.handleSelectMenu('page2')}}>
+              <ListItemText primary="Page2" />
+            </ListItem>
+          </List>
+        </Drawer>
+      );
+  }
+}
+
+export default withStyles(styles)(LeftMenu);
