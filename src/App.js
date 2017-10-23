1312eeb376ff1b9a5c105fc69a5ccd7f9e8b8b21
yoskeoka <yoske95@gmail.com>
2017-10-24 02:00:32 +0900

    prettier

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-import React from 'react';
-import MyAppBar from './components/MyAppBar';
+import React from "react";
+import MyAppBar from "./components/MyAppBar";
 import LeftMenu from "./components/LeftMenu";
-import GithubStarRating from './components/GithubStarRating';
-import './App.css';
+import GithubStarRating from "./components/GithubStarRating";
+import "./App.css";
 
 class App extends React.Component {
-  state ={
-    leftDrawerOpen: false,
-    page: "nothing",
-  }
+    state = {
+        leftDrawerOpen: false,
+        page: "nothing",
+    };
 
-  toggleLeftMenu = (open) => {
-    this.setState({
-      leftDrawerOpen: open,
-    });
-  }
+    toggleLeftMenu = open => {
+        this.setState({
+            leftDrawerOpen: open,
+        });
+    };
 
-  selectMenu = (page)=>{
-    this.toggleLeftMenu(false);
-    // change content
-    this.setState({page: page})
-  }
+    selectMenu = page => {
+        this.toggleLeftMenu(false);
+        // change content
+        this.setState({ page: page });
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <MyAppBar title="gcoka Playground" onClickMenu={()=>this.toggleLeftMenu(true)} />
-        <LeftMenu
-          open={this.state.leftDrawerOpen}
-          handleClose={()=>this.toggleLeftMenu(false)}
-          handleSelectMenu={this.selectMenu}></LeftMenu>
+    render() {
+        return (
+            <div className="App">
+                <MyAppBar title="gcoka Playground" onClickMenu={() => this.toggleLeftMenu(true)} />
+                <LeftMenu
+                    open={this.state.leftDrawerOpen}
+                    handleClose={() => this.toggleLeftMenu(false)}
+                    handleSelectMenu={this.selectMenu}
+                />
 
-        {/* TODO: use react-rounter */}
-        <p className="App-intro">
-          {this.state.page} is Selected!
-        </p>
-        {
-          this.state.page === "github-star-rating" ?
-            <GithubStarRating /> : null
-        }
-      </div>
-    );
-  }
+                {/* TODO: use react-rounter */}
+                <p className="App-intro">{this.state.page} is Selected!</p>
+                {this.state.page === "github-star-rating" ? <GithubStarRating /> : null}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import { withStyles } from 'material-ui/styles';
-import Drawer from 'material-ui/Drawer';
-import List, { ListItem, ListItemText } from 'material-ui/List';
+import React from "react";
+import { withStyles } from "material-ui/styles";
+import Drawer from "material-ui/Drawer";
+import List, { ListItem, ListItemText } from "material-ui/List";
 
 const styles = {
-  list: {
-    width: 250,
-    flex: 'initial',
-  }
+    list: {
+        width: 250,
+        flex: "initial",
+    },
 };
 
 class LeftMenu extends React.Component {
-  render(){
-      return (
-        <Drawer open={this.props.open} onRequestClose={this.props.handleClose}>
-          <List className={this.props.classes.list}>
-            <ListItem button onClick={()=>{this.props.handleSelectMenu('github-star-rating')}}>
-              <ListItemText primary="Github Star Rating" secondary="Search for Javascript" />
-            </ListItem>
-            <ListItem button onClick={()=>{this.props.handleSelectMenu('page2')}}>
-              <ListItemText primary="Page2" />
-            </ListItem>
-          </List>
-        </Drawer>
-      );
-  }
+    render() {
+        return (
+            <Drawer open={this.props.open} onRequestClose={this.props.handleClose}>
+                <List className={this.props.classes.list}>
+                    <ListItem
+                        button
+                        onClick={() => {
+                            this.props.handleSelectMenu("github-star-rating");
+                        }}>
+                        <ListItemText primary="Github Star Rating" secondary="Search for Javascript" />
+                    </ListItem>
+                    <ListItem
+                        button
+                        onClick={() => {
+                            this.props.handleSelectMenu("page2");
+                        }}>
+                        <ListItemText primary="Page2" />
+                    </ListItem>
+                </List>
+            </Drawer>
+        );
+    }
 }
 
 export default withStyles(styles)(LeftMenu);
